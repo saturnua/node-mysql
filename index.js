b0058008c6ac7383bc04c62172e0a4b3d31447eb
Ievhen Perekhodchenko <i.perekhodchenko@favbet.com>
2021-01-26 16:55:47 +0200

    Add working with GraphQl, all tasks done

--- index.js
+++ index.js
@@ -1,13 +1,20 @@
 const express = require('express')
 const path = require('path')
-const todoRoutes = require('./routes/todo')
 const sequelize = require('./utils/database')
+const {graphqlHTTP} = require('express-graphql')
+const schema = require('./graphql/schema')
+const resolver = require('./graphql/resolver')
 const app = express()
 const PORT = process.env.PORT || 3000
 
 app.use(express.static(path.join(__dirname, 'public')))
 app.use(express.json())
-app.use('/api/todo', todoRoutes)
+
+app.use(graphqlHTTP({
+    schema: schema,
+    rootValue: resolver,
+    graphiql: true
+}))
 
 app.use((req, res) => {
     res.sendFile('/index.html')
